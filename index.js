2fc0f4b1fc522245fae93feec4bf9f3fb80e7cff
Pial <pialname394@gmail.com>
2026-05-21 09:05:52 +0600

    successfully connected mongodb drivers

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require("express");
 const cors = require("cors");
-
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
+const port = process.env.PORT;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const port = 5000;
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from backend");
 });
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
