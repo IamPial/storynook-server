dc76e6f1b259aaa3983c0cd0ed88b29bed60e2e3
Pial <pialname394@gmail.com>
2026-05-31 22:43:39 +0600

    comments some line

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("storynook");
     const addRoomCollection = db.collection("AddRoom");
@@ -227,7 +227,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
